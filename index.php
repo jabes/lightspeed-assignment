688a984c9db4de9776aea2be20d859d6419d134a
Justin Bull <bull.justin@gmail.com>
2019-12-09 20:38:49 -0800

    Use day as array index

--- index.php
+++ index.php
@@ -6,27 +6,28 @@ class TwelveDaysChristmas {
 
   function __construct() {
     $this->buildGiftList();
+    var_dump($this->gifts);
   }
 
-  private function addGift(string $gift) {
-    $previousGift = end($this->gifts) ?: [];
-    array_unshift($previousGift, $gift);
-    array_push($this->gifts, $previousGift);
+  private function addGift(int $day, string $item) {
+    $newGift = end($this->gifts) ?: [];
+    array_unshift($newGift, $item);
+    $this->gifts[$day] = $newGift;
   }
 
   private function buildGiftList() {
-    $this->addGift("A partridge in a pear tree");
-    $this->addGift("Two turtle doves");
-    $this->addGift("Three French hens");
-    $this->addGift("Four calling birds");
-    $this->addGift("Five golden rings");
-    $this->addGift("Six geese a-laying");
-    $this->addGift("Seven swans a-swimming");
-    $this->addGift("Eight maids a-milking");
-    $this->addGift("Nine ladies dancing");
-    $this->addGift("Ten lords a-leaping");
-    $this->addGift("Eleven pipers piping");
-    $this->addGift("Twelve drummers drumming");
+    $this->addGift(1, "A partridge in a pear tree");
+    $this->addGift(2, "Two turtle doves");
+    $this->addGift(3, "Three French hens");
+    $this->addGift(4, "Four calling birds");
+    $this->addGift(5, "Five golden rings");
+    $this->addGift(6, "Six geese a-laying");
+    $this->addGift(7, "Seven swans a-swimming");
+    $this->addGift(8, "Eight maids a-milking");
+    $this->addGift(9, "Nine ladies dancing");
+    $this->addGift(10, "Ten lords a-leaping");
+    $this->addGift(11, "Eleven pipers piping");
+    $this->addGift(12, "Twelve drummers drumming");
   }
 }
 
