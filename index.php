61733e3cc17f9aec2d2120a598c073049d8881a5
Justin Bull <bull.justin@gmail.com>
2019-12-09 19:58:48 -0800

    Add array of gifts

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+$gifts = [
+  1 => ["A partridge in a pear tree"],
+  2 => ["Two turtle doves", "A partridge in a pear tree"],
+  3 => ["Three French hens", "Two turtle doves", "A partridge in a pear tree"],
+  4 => ["Four calling birds", "Three French hens", "Two turtle doves", "A partridge in a pear tree"],
+  5 => ["Five gold rings", "Four calling birds", "Three French hens", "Two turtle doves", "A partridge in a pear tree"],
+  6 => ["Six geese a laying", "Five gold rings", "Four calling birds", "Three French hens", "Two turtle doves", "A partridge in a pear tree"],
+  7 => ["Seven swans a swimming", "Six geese a laying", "Five gold rings", "Four calling birds", "Three French hens", "Two turtle doves", "A partridge in a pear tree"],
+  8 => ["Eight maids a milking", "Seven swans a swimming", "Six geese a laying", "Five gold rings", "Four calling birds", "Three French hens", "Two turtle doves", "A partridge in a pear tree"],
+  9 => ["Nine drummers drumming"],
+  10 => ["Ten pipers piping", "Nine drummers drumming", "Eight maids a milking", "Seven swans a swimming", "Six geese a laying", "Five gold rings", "Four calling birds", "Three French hens", "Two turtle doves", "A partridge in a pear tree"],
+  11 => ["Eleven ladies dancing", "Ten pipers piping", "Nine drummers drumming", "Eight maids a milking", "Seven swans a swimming", "Six geese a laying", "Five gold rings", "Four calling birds", "Three French hens", "Two turtle doves", "A partridge in a pear tree"],
+  12 => ["Twelve Lords a leaping", "Eleven ladies dancing", "Ten pipers piping", "Nine drummers drumming", "Eight maids a milking", "Seven swans a swimming", "Six geese a laying", "Five gold rings", "Four calling birds", "Three French hens", "Two turtle doves", "A partridge in a pear tree"],
+];
+
+var_dump($gifts);
