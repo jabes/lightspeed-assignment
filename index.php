83f80d2a78366ad2bc09e1f27e11364059a12c3f
Justin Bull <bull.justin@gmail.com>
2019-12-09 20:30:25 -0800

    Build gift list procedurally

--- index.php
+++ index.php
@@ -1,18 +1,33 @@
 <?php
 
-$gifts = [
-  1 => ["A partridge in a pear tree"],
-  2 => ["Two turtle doves", "A partridge in a pear tree"],
-  3 => ["Three French hens", "Two turtle doves", "A partridge in a pear tree"],
-  4 => ["Four calling birds", "Three French hens", "Two turtle doves", "A partridge in a pear tree"],
-  5 => ["Five gold rings", "Four calling birds", "Three French hens", "Two turtle doves", "A partridge in a pear tree"],
-  6 => ["Six geese a laying", "Five gold rings", "Four calling birds", "Three French hens", "Two turtle doves", "A partridge in a pear tree"],
-  7 => ["Seven swans a swimming", "Six geese a laying", "Five gold rings", "Four calling birds", "Three French hens", "Two turtle doves", "A partridge in a pear tree"],
-  8 => ["Eight maids a milking", "Seven swans a swimming", "Six geese a laying", "Five gold rings", "Four calling birds", "Three French hens", "Two turtle doves", "A partridge in a pear tree"],
-  9 => ["Nine drummers drumming"],
-  10 => ["Ten pipers piping", "Nine drummers drumming", "Eight maids a milking", "Seven swans a swimming", "Six geese a laying", "Five gold rings", "Four calling birds", "Three French hens", "Two turtle doves", "A partridge in a pear tree"],
-  11 => ["Eleven ladies dancing", "Ten pipers piping", "Nine drummers drumming", "Eight maids a milking", "Seven swans a swimming", "Six geese a laying", "Five gold rings", "Four calling birds", "Three French hens", "Two turtle doves", "A partridge in a pear tree"],
-  12 => ["Twelve Lords a leaping", "Eleven ladies dancing", "Ten pipers piping", "Nine drummers drumming", "Eight maids a milking", "Seven swans a swimming", "Six geese a laying", "Five gold rings", "Four calling birds", "Three French hens", "Two turtle doves", "A partridge in a pear tree"],
-];
+class TwelveDaysChristmas {
 
-var_dump($gifts);
+  private $gifts = [];
+
+  function __construct() {
+    $this->buildGiftList();
+  }
+
+  private function addGift(string $gift) {
+    $previousGift = end($this->gifts) ?: [];
+    array_unshift($previousGift, $gift);
+    array_push($this->gifts, $previousGift);
+  }
+
+  private function buildGiftList() {
+    $this->addGift("A partridge in a pear tree");
+    $this->addGift("Two turtle doves");
+    $this->addGift("Three French hens");
+    $this->addGift("Four calling birds");
+    $this->addGift("Five golden rings");
+    $this->addGift("Six geese a-laying");
+    $this->addGift("Seven swans a-swimming");
+    $this->addGift("Eight maids a-milking");
+    $this->addGift("Nine ladies dancing");
+    $this->addGift("Ten lords a-leaping");
+    $this->addGift("Eleven pipers piping");
+    $this->addGift("Twelve drummers drumming");
+  }
+}
+
+$worker = new TwelveDaysChristmas();
